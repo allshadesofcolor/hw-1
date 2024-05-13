b20c587f519b9ce0b576ea1a44d2dd3c94c68cdc
allshadesofcolor <163556067+allshadesofcolor@users.noreply.github.com>
2024-05-12 19:44:10 -0500

    First commit

---
+++ hw_assignments.py
@@ -0,0 +1,82 @@
+#Task 1 Answer 1 Showcase your moves
+
+weather = "rain"
+
+if weather == "Sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2 Answer 1 Your mood
+
+user_input = (input("How do you feel today?"))
+if input == "Happy":
+    print("That's great to hear!")
+if input == "Sad":
+    print("I hope your day gets better!")
+
+
+#Task 2 Answer 1 Python naming convention
+
+Pi_value = 3.14195
+user_age = 25
+user_location = "New York"
+MAXLIMIT = 1000
+
+
+#Task 3 Answer 1 Type function
+variable_a = "Hello, World!"
+print(type(variable_a))
+
+
+variable_b = 23
+print(type ("varible_b"))
+
+
+variable_c = 3.14
+print(type (variable_c))
+
+
+variable_d = True
+print(type (variable_d))
+
+#Task 4 Answer 1 Grocery store math
+
+Bread = 4.99
+Peanut_butter = 5.99
+Jelly = 3.99
+Price = (Bread, + Peanut_butter, + Jelly)
+print(sum(Price))
+
+#Task 4 Answer 2 Bank Interest
+
+savings_account = 10000
+interest_rate = .07
+balance = (savings_account * interest_rate)
+print(balance)
+
+#Task 5 Value Swapping
+
+x = 5
+y = 7
+Total = (x + y)
+print(Total)
+
+x = 7
+y = 5
+Total = (x + y)
+print(Total)
+
+
+#Task 6 Answer 1 Validating Calculations
+
+num1 = 25
+num2 = 50
+Total = (num1 + num2)
+print(Total)
+
+num3 = 10
+num4 = 20
+Total = num3 + num4
+print(Total)
+
